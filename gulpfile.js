6889a89869581eea7a80c145b857a97cc8604e24
Maria J Correa <mjcorreahudson@gmail.com>
2018-06-15 23:14:12 +0200

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+/*eslint-env node */
+/*eslint linebreak-style: ["error", "windows"]*/
+/*eslint quotes: ["error", "single"]*/
+/*eslint indent: 1 */
+
+var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+
+
+gulp.task('test', function () {
+    // This will only run if the lint task is successful...
+    gulp.watch('jasmine/**/*.js', gulp.series('lint'));
+});
+gulp.task('lint', () => {
+    // ESLint ignores files with "node_modules" paths.
+    // So, it's best to have gulp ignore the directory as well.
+    // Also, Be sure to return the stream from the task;
+    // Otherwise, the task may end before the stream has finished.
+    return gulp.src(['**/*.js','!node_modules/**'])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
+});
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,3 +1,6 @@
+/*eslint no-undef: 0*/
+/*eslint linebreak-style: ["error", "windows"]*/
+
 /* feedreader.js
  *
  * This is the spec file that Jasmine will read and contains
@@ -24,6 +27,7 @@ $(function() {
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
+
         });
 
 
@@ -31,42 +35,112 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('has urls', function() {
+            allFeeds.forEach(function(feed) {
+                var url = feed.url;
+
+                expect(url.length).not.toBe(0);
+            });
+        });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('has name', function() {
+            allFeeds.forEach(function(feed) {
+                var name = feed.name;
+
+                expect(name.length).not.toBe(0);
+            });
+        });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
 
+    describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
 
-         /* TODO: Write a test that ensures the menu changes
+        const slideMenu = document.querySelector('.slide-menu');
+        let parentClassName = slideMenu.parentElement.className;
+
+
+
+        it('is hidden', function() {
+            expect(parentClassName).toBe('menu-hidden');
+
+        });
+        /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        it('menu hides when clicking', function() {
+            const menuIcon = $('.menu-icon-link');
+            function click() {
+                menuIcon.on('click', function() {
+                    return (slideMenu.parentElement.className);
+                });
+            }
+            expect(click()).not.toBe(parentClassName);
+
+        });
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
 
+        it('verifies that there is at least one entry', function(done) {
+            const entryText = document.querySelector('.feed .entry').innerText;
+            expect(entryText.length).toBeGreaterThan(0);
+            done();
+        });
+
+    });
     /* TODO: Write a new test suite named "New Feed Selection" */
-
+    describe('New Feed Selection', function() {
+        try {
+            let newEntryText = document.querySelector('.feed .entry').innerText;
+        } catch(err) {
+            newEntryText=0;
+        }
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        beforeEach(function() {
+            if (newEntryText === null) {
+                newEntryText = 0;
+            }
+        });
+        afterEach(function(done) {
+            loadFeed(0);
+            done();
+        });
+
+        it('the content changed', function(done) {
+            // same querySelector as newEntryText, but with another name so as to not create double assignment
+            const anotherEntryText = document.querySelector('.feed .entry').innerText;
+            // so that newEntryText and anotherEntryText are the same querySelectors called before and after completing the async function
+            expect(newEntryText).not.toBe(anotherEntryText);
+            done();
+        });
+
+
+    });
 }());
